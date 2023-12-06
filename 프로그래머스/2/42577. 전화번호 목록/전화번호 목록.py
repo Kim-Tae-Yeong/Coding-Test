fdf84b872a74f141c2712b6012f001f9cdf0296a
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2023-12-06 11:55:36 +0900

    [level 2] Title: 전화번호 목록, Time: 112.92 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -4,8 +4,8 @@ def solution(phone_book):
     for i in range(length - 1):
         check = len(phone_book[i])
         for j in range(i + 1, length):
-            if(phone_book[i][0] != phone_book[j][0]):
-                break
+            # if(phone_book[i][0] != phone_book[j][0]):
+            #     break
             if(phone_book[j][: check] > phone_book[i]):
                 break
             elif(phone_book[j][: check] == phone_book[i]):
