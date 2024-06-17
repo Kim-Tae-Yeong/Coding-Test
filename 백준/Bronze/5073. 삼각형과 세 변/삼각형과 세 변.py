e166502bd8dc04a0e3a4e9ada7487f831725a6c4
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-06-17 20:10:37 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -0,0 +1,23 @@
+ans = []
+
+while True:
+  tmp = [0, 0, 0]
+  a, b, c = map(int, input().split())
+  tmp[0] = a
+  tmp[1] = b
+  tmp[2] = c
+  tmp.sort(reverse = True)
+  if(a == 0):
+    break
+  if(tmp[0] >= tmp[1] + tmp[2]):
+    ans.append("Invalid")
+  elif(tmp[0] == tmp[2]):
+    ans.append("Equilateral")
+  elif(tmp[0] == tmp[1] or tmp[1] == tmp[2]):
+    ans.append("Isosceles")
+  else:
+    ans.append("Scalene")
+  
+
+for elem in ans:
+  print(elem)
