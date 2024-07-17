1dadbe992f16b81e494cf5fb0f58c4c0058efd6b
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-07-17 23:56:36 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.py
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.py
@@ -1,26 +1,21 @@
-def dfs(r, c, prev_dir, cost):
-    if r == n - 1:
-        candidate.append(cost)
-        return
-    
-    # 현재 위치에서 다음 위치로 이동
-    for direction in range(3):
-        nr = r + 1
-        nc = c + (direction - 1)
-        
-        # 새로운 좌표가 유효한지 확인
-        if 0 <= nc < m and direction != prev_dir:
-            new_cost = cost + board[nr][nc]
-            dfs(nr, nc, direction, new_cost)
+def dfs(r, c, d, cost):
+  if(r == n - 1):
+    candidate.append(cost)
+    return 0
+  
+  for nd in range(3):
+    nr = r + 1
+    nc = c + (nd - 1)
+    if(0 <= nc < m and d != nd):
+      new_cost = cost + board[nr][nc]
+      dfs(nr, nc, nd, new_cost)
 
-# 입력 받기
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
 candidate = []
 
-# 첫 번째 행에서 시작
-for col in range(m):
-    dfs(0, col, -1, board[0][col])
+for dcol in range(m):
+  dfs(0, dcol, -1, board[0][dcol])
 
 print(min(candidate))
