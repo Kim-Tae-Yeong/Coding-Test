4000df48cc56f6d8c32ce4ff195d457dfd82ded1
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-07-23 15:46:03 +0900

    [Silver II] Title: 에디터, Time: 348 ms, Memory: 41540 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,22 @@
+import sys
+
+word = list(input())
+m = int(input())
+left_stack = word
+right_stack = []
+
+for _ in range(m):
+  command = list(sys.stdin.readline().strip().split())
+  if(command[0] == 'L'):
+    if(left_stack):
+      right_stack.append(left_stack.pop())
+  elif(command[0] == 'D'):
+    if(right_stack):
+      left_stack.append(right_stack.pop())
+  elif(command[0] == 'B'):
+    if(left_stack):
+      left_stack.pop()
+  elif(command[0] == 'P'):
+    left_stack.append(command[1])
+
+print("".join(left_stack + right_stack[::-1]))
