fd2e6198f41f042bf56330a56285bebb745cbceb
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-08-03 21:15:01 +0900

    [Silver V] Title: 영화감독 숌, Time: 600 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,24 +1,11 @@
 n = int(input())
-start = 666
-ans = 0
+num = 666
+cnt = 0
 while True:
-  num = start
-  cnt = 0
-  check = 0
-  while True:
-    if(check >= 3):
-      ans += 1
-      break
-    if(num == 0):
-      break
-    tmp = num % 10
-    if(tmp == 6):
-      check += 1
-    else:
-      check = 0
-    num //= 10
-  if(ans == n):
+  if('666' in str(num)):
+    cnt += 1
+  if(cnt == n):
     break
-  start += 1
+  num += 1
 
-print(start)
+print(num)
