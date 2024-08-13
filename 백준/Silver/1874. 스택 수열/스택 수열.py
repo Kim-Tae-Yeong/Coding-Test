ffceb143ac3701c0ccad7f8a8fea32f047377e34
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-08-13 14:21:25 +0900

    [Silver II] Title: 스택 수열, Time: 4144 ms, Memory: 33488 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+s = []
+ans = []
+cnt = 1
+possible = True
+for _ in range(n):
+  num = int(input())
+  if(len(s) == 0):
+    for i in range(cnt, num + 1):
+      s.append(i)
+      ans.append('+')
+    cnt = num + 1
+  elif(num >= cnt):
+    for i in range(cnt, num + 1):
+      s.append(i)
+      ans.append('+')
+    cnt = num + 1
+  if(s[-1] == num):
+    s.pop()
+    ans.append('-')
+  else:
+    possible = False
+
+if(not possible):
+  print('NO')
+else:
+  print('\n'.join(ans))
