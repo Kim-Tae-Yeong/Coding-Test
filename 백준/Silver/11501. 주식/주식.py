5e21989e3235224084f8006ad79fc81ee542d89e
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-08-19 23:58:52 +0900

    [Silver II] Title: 주식, Time: 2780 ms, Memory: 170912 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,22 +1,18 @@
 import sys
+# sys.stdin = open('/Users/kimtaeyeong/BaekJoon/11501/11501.txt', 'rt')
 
-t = int(input())
+t = int(sys.stdin.readline().strip())
 ans = []
-
 for _ in range(t):
   n = int(sys.stdin.readline().strip())
-  price = list(map(int, sys.stdin.readline().strip().split()))
-
-  max_price = [0 for _ in range(n)]
-  max_price[-1] = price[-1]
+  day = list(map(int, sys.stdin.readline().strip().split()))
+  score = 0
+  max_num = day[-1]
   for i in range(n - 2, -1, -1):
-    max_price[i] = max(price[i], max_price[i + 1])
+    if(day[i] < max_num):
+      score += (max_num - day[i])
+    else:
+      max_num = day[i]
+  ans.append(str(score))
 
-  total = 0
-  for i in range(n):
-    if(max_price[i] > price[i]):
-      total += max_price[i] - price[i]
-
-  ans.append(str(total))
-  
-print("\n".join(ans))
+print('\n'.join(ans))
