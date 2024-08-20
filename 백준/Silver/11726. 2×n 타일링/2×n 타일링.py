38d6d5465555dc9e2e9e85848d216142756061b4
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-08-21 00:43:48 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,15 @@
+import sys
+# sys.stdin = open('/Users/kimtaeyeong/BaekJoon/11726/11726.txt', 'rt')
+
+n = int(sys.stdin.readline().strip())
+if(n == 1):
+  print(1)
+elif(n == 2):
+  print(2)
+else:
+  dp = [0 for _ in range(n + 1)]
+  dp[1] = 1
+  dp[2] = 2
+  for i in range(3, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+  print(dp[-1] % 10007)
