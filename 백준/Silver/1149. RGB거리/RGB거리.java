b52a34842af0fbd895449542b90cc591a1da1c40
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-09-08 21:04:12 +0900

    [Silver I] Title: RGB거리, Time: 132 ms, Memory: 15328 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  public static int n;
+  public static List<List<Integer>> cost = new ArrayList<>();
+  public static int[] dp = new int[3];
+  public static int[] board = new int[3];
+  public static int ans = Integer.MAX_VALUE;
+
+  public static void main (String[] args) throws IOException {
+    // BufferedReader br = new BufferedReader(new FileReader("/Users/kimtaeyeong/CodingTest/BaekJoon/1149/1149.txt"));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    n = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < n; i++) {
+      String[] input = br.readLine().split(" ");
+      List<Integer> tmp = new ArrayList<>();
+      for (int j = 0; j < 3; j++) {
+        tmp.add(Integer.parseInt(input[j]));
+      }
+      cost.add(tmp);
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (i == 0) {
+          dp[j] = cost.get(0).get(j);
+        }
+        else {
+          int tmp = Integer.MAX_VALUE;
+          for (int k = 0; k < 3; k++) {
+            if (j != k) {
+              tmp = Math.min(tmp, board[k]);
+            }
+          dp[j] = tmp + cost.get(i).get(j);
+          }
+        }
+      }
+      for (int j = 0; j < 3; j++) {
+        board[j] = dp[j];
+      }
+    }
+    
+    for (int i = 0; i < 3; i++) {
+      ans = Math.min(ans, board[i]);
+    }
+
+    System.out.println(ans);
+
+    br.close();
+  }
+}
