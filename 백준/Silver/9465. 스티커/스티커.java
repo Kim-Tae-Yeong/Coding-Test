bb25f7dc87a021280571d00595e4f78b6b0d1265
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-09-10 13:09:03 +0900

    Update 스티커.java

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -26,10 +26,14 @@ public class Main {
       }
 
       for (int j = 1; j < n + 1; j++) {
+        // dp의 첫번째 col은 board 값 그대로 가져옴
         if (j == 1) {
           dp[0][1] = board[0][0];
           dp[1][1] = board[1][0];
         }
+        // 이후 col은 두 가지 경우를 비교
+        // 1) 현재 col 바로 직전 col( = col - 1)에서 오는 경우 -> 현재 row의 대각선에 위치한 값에서 현재 값을 더함
+        // 2) (col - 2)에서 오는 경우 -> 1)처럼 꼭 대각선에서 올 필요가 없기 때문에 (col - 2)에 위치한 값 중 더 큰 값에 현재 값을 더함
         else {
           dp[0][j] = Math.max(dp[1][j - 1] + board[0][j - 1], Math.max(dp[0][j - 2], dp[1][j - 2]) + board[0][j - 1]);
           dp[1][j] = Math.max(dp[0][j - 1] + board[1][j - 1], Math.max(dp[0][j - 2], dp[1][j - 2]) + board[1][j - 1]);
