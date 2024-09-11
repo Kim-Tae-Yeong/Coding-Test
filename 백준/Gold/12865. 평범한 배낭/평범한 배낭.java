3a46e951e08f7b87a2be1f77f902b5ef73b7401d
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-09-11 23:49:25 +0900

    Update 평범한 배낭.java

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public class Main {
   public static int n, k;
-  // 물건을 무게를 기준으로 오름차순 정렬
   public static List<int[]> info = new ArrayList<>();
   // ans[r][c] : 0 ~ (r - 1)까지의 물건을 사용해서 무게 c를 만들었을 때 최대 가치
   public static int[][] ans;
@@ -28,6 +27,7 @@ public class Main {
       info.add(new int[] {w, v});
     }
 
+    // 물건을 무게를 기준으로 오름차순 정렬
     info.sort((o1, o2) -> o1[0] - o2[0]);
 
     ans = new int[n + 1][k + 1];
@@ -38,9 +38,13 @@ public class Main {
       int currentValue = info.get(i)[1];
       // 무게를 1씩 늘림
       for (int j = 1; j < k + 1; j++) {
+        // 현재 물건의 무게가 가방의 무게보다 무거우면 현재 물건을 가방에 넣지 않음
+        // 즉 현재 가방의 무게에 대해 이전 물건까지 계산한 값을 가져옴
         if (currentWeigth > j) {
           ans[i + 1][j] = ans[i][j];
         }
+        // 그렇지 않으면( = 현재 물건을 가방에 넣을 수 있으면)
+        // 현재 물건을 가방에 넣었을 때 가치와 넣지 않았을 때의 가치 중 더 큰 값을 가져옴
         else {
           ans[i + 1][j] = Math.max(ans[i][j - currentWeigth] + currentValue, ans[i][j]);
         }
