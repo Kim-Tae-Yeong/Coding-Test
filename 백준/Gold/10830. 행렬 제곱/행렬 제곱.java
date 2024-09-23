4a39893d7069838f570dc5f6e96b155d41690d11
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-09-23 11:22:30 +0900

    Update 행렬 제곱.java

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
+// import java.io.FileReader;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main_10830 {
   private static int n;
+  // b는 int 크기를 넘어가기 때문에 long으로 받음
   private static long b;
   private static int[][] map;
 
+  // matrix의 각 원소 계산
   private static int[][] cal (int[][] a, int[][] b) {
     int[][] tmp = new int[n][n];
     int row = 0;
@@ -19,6 +21,8 @@ public class Main {
       for (int i = 0; i < n; i++) {
         elem += a[row][i] * b[i][col];
       }
+      // tmp의 원소는 int이기 때문에 int가 넘어버리는 숫자가 원소로 들어가면 음수 등장
+      // 때문에 여기서도 값을 나눠줌
       tmp[row][col] = elem % 1000;
       if (col == n - 1) {
         col = 0;
@@ -31,18 +35,24 @@ public class Main {
     return tmp;
   }
 
+  // A ^ n = A ^ (n / 2) * A (n / 2)
   private static int[][] matrix (long idx) {
     if (idx == 1) {
       return map;
     }
     else {
+      // idx가 홀수인지 확인
       boolean check = false;
       if (idx % 2 == 1) {
         check = true;
       }
       idx = idx / 2;
+      // idx를 반으로 나눈 matrix를 가져옴
       int[][] half = matrix(idx);
+      // 해당 matrix 제곱 계산
       int[][] ans = cal(half, half);
+      // idx가 홀수이면, A ^ n = A ^ (n - 1) * A
+      // idx가 홀수이면 입력받은 matrix를 한번 더 곱해줌
       if (check) {
         ans = cal(ans, map);
       }
