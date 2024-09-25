80d7e79227503ac27cb6b57bff258dd10c32cb00
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-09-25 10:37:50 +0900

    Update 플로이드.java

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,17 +1,20 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
+// import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Main {
+public class Main_11404 {
   private static int n, m;
+  // graph[i][j] : i에서 j로 한번에 가는 경로 중 최소 비용
   private static int[][] graph;
+  // map[i][j] : i에서 j로 가는 경로 중 최소 비용
   private static int[][] map;
   private static StringBuilder ans = new StringBuilder();
 
   private static void dijkstra(int s) {
     Queue<int[]> q = new LinkedList<>();
+    // s에서 도착할 수 있는 도시와 비용 저장
     for(int i = 1; i < n + 1; i++) {
       if(graph[s][i] != 0) {
         q.add(new int[] {s, i, graph[s][i]});
@@ -22,14 +25,18 @@ public class Main {
       int start = info[0];
       int end = info[1];
       int cost = info[2];
+      // 도착 도시에 한번도 방문을 하지 않았거나, 더 작은 비용으로 갈 수 있는 경우 비용 갱신
       if(map[s][end] == 0 || map[s][start] + cost < map[s][end]) {
         map[s][end] = map[s][start] + cost;
       }
+      // 도착 도시에서 갈 수 있는 도시 탐색
       for(int i = 1; i < n + 1; i++) {
+        // 시작 도시와 같으면 continue
         if(i == s) {
           continue;
         }
         if(graph[end][i] != 0) {
+          // 갈 수 있는 도시 중 한번도 방문하지 않은 도시나, 더 작은 비용으로 갈 수 있는 경우 queue에 추가
           if(map[s][i] == 0 || map[s][end] + graph[end][i] < map[s][i]) {
             q.add(new int[] {end, i, graph[end][i]});
           }
@@ -53,11 +60,12 @@ public class Main {
       int start = Integer.parseInt(input[0]);
       int end = Integer.parseInt(input[1]);
       int cost = Integer.parseInt(input[2]);
+      // start에서 end로 가는 비용 중 최소 비용만 저장
       if(graph[start][end] == 0 || cost < graph[start][end]) {
         graph[start][end] = cost;
       }
     }
-
+    
     for (int i = 1; i < n + 1; i++) {
       dijkstra(i);
     }
