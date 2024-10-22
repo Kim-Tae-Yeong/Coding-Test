89463199d39e7cc8923e08ce966803bfb7910ef0
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-10-22 20:15:30 +0900

    Update 폰켓몬.java

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,12 +4,15 @@ class Solution {
     private HashSet<Integer> h = new HashSet();
     public int solution(int[] nums) {
         int answer = 0;
+        // 입력받은 배열에서 폰켓몬의 종류가 몇가지인지 구함
         for(int elem : nums) {
             h.add(elem);
         }
+        // 폰켓몬의 종류가 N / 2보다 많으면 N / 2를 정답으로 취함
         if(h.size() > nums.length / 2) {
             answer = nums.length / 2;
         }
+        // 그것보다 작을 경우 폰켓몬의 종류 개수를 정답으로 취함
         else {
             answer = h.size();
         }
