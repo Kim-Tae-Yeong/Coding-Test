8d80a7fbcdd74e0b4aa201c923f56efd007c3f2f
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-10-26 21:46:35 +0900

    Update 전화번호 목록.java

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 class Solution {
@@ -10,6 +9,8 @@ class Solution {
         for(String elem : phone_book) {
           l.add(elem);
         }
+        // 전화번호부를 문자열 순으로 오름차순 정렬
+        // 때문에 이웃된 두 전화번호만 비교하면 됨
         l.sort(null);
         for(int i = 0; i < phone_book.length - 1; i++) {
           if(l.get(i + 1).startsWith(l.get(i))) {
