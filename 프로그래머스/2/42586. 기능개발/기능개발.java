a66ea0da1b708d77a3ea843a4c89f8b204faee92
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-10-27 20:09:59 +0900

    Update 기능개발.java

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -15,14 +15,17 @@ class Solution {
             if (idx == length) {
                 break;
             }
+            // 하루씩 progress를 진행시킴
             for (int i = idx; i < length; i++) {
                 progresses[i] += speeds[i];
             }
             tmp = 0;
             while (true) {
+                // 0부터 시작해서 100이 넘는 progress까지의 개수를 구함
                 if (progresses[idx] >= 100) {
                     tmp += 1;
                     idx += 1;
+                    // 마지막 progress까지 확인했으면 break
                     if (idx == length) {
                         break;
                     }
@@ -30,6 +33,7 @@ class Solution {
                     break;
                 }
             }
+            // 해당 날에 100이 넘는 progress가 있으면 정답에 추가
             if (tmp != 0) {
                 ans.add(tmp);
             }
