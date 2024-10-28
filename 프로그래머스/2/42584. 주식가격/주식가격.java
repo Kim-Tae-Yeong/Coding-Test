97a608b1f6273eedee3fd17ae5e3f2907925f7a7
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-10-28 11:39:02 +0900

    Update 주식가격.java

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,9 +5,12 @@ class Solution {
 
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        // i번째 주식에 대해 (i + 1) ~ (price.length - 1) 주식까지 확인
         for (int i = 0; i < prices.length - 1; i++) {
             for (int j = i + 1; j < prices.length; j++) {
+                // 다음 주식 값에 상관없이 1초 늘어남
                 answer[i] += 1;
+                // 다음 주식 값이 더 작으면 시간이 더 이상 늘어나지 않음
                 if (prices[i] > prices[j]) {
                     break;
                 }
