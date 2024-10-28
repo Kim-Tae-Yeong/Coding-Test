8ccce78c753e39adb3fc87c0be6dfc46a44f46b3
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-10-28 23:44:10 +0900

    [level 3] Title: 이중우선순위큐, Time: 105.21 ms, Memory: 146 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,33 @@
+import java.util.TreeMap;
+
+class Solution {
+    
+
+    public int[] solution(String[] operations) {
+        int[] answer = new int[2];
+        TreeMap<Integer, Integer> tm = new TreeMap<>();
+        for (String elem : operations) {
+            String[] cmd = elem.split(" ");
+            if (cmd[0].equals("I")) {
+                tm.put(Integer.parseInt(cmd[1]), tm.getOrDefault(Integer.parseInt(cmd[1]), 0) + 1);
+            } else if (!tm.isEmpty()) {
+                if (cmd[1].equals("1")) {
+                    tm.replace(tm.lastKey(), tm.get(tm.lastKey()) - 1);
+                    if (tm.get(tm.lastKey()).equals(0)) {
+                        tm.remove(tm.lastKey());
+                    }
+                } else {
+                    tm.replace(tm.firstKey(), tm.get(tm.firstKey()) - 1);
+                    if (tm.get(tm.firstKey()).equals(0)) {
+                        tm.remove(tm.firstKey());
+                    }
+                }
+            }
+        }
+        if (!tm.isEmpty()) {
+            answer[0] = tm.lastKey();
+            answer[1] = tm.firstKey();
+        }
+        return answer;
+    }
+}
