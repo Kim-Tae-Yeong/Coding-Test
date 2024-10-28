199baf20739e77bff27ab688f358520772d30467
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-10-28 19:43:24 +0900

    Update 더 맵게.java

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -10,9 +10,11 @@ class Solution {
             pq.add(elem);
         }
         while (true) {
+            // 가장 맵지 않은 음식의 스코빌이 K를 넘으면 break
             if (pq.peek() >= K) {
                 break;
             }
+            // 음식이 하나밖에 남지 않으면, 해당 음식으로 스코빌을 K를 넘길 수 없기 때문에 -1 return
             if (pq.size() == 1) {
                 answer = -1;
                 break;
