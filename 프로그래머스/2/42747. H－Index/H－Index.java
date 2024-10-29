25310695ccb5c1607d16e9dbd1cfdb2b7c987661
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-10-29 22:00:12 +0900

    Update H－Index.java

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -7,15 +7,20 @@ class Solution {
     
 
     public int solution(int[] citations) {
+        // answer : 논문 인용 횟수
         int answer = 0;
         List<Integer> l = new ArrayList<>();
         for (int elem : citations) {
             l.add(elem);
         }
+        // 논문 인용 횟수 오름차순 정렬
         l.sort(null);
+        // idx : 현재 확인한 논문의 index
         int idx = 0;
         while (true) {
+            // 현재 answer(= h)가 배열의 논문 인용 횟수와 같으면
             if (l.get(idx) == answer) {
+                // 다음 논문 인용 횟수로 넘어감
                 while (true) {
                     if (idx == citations.length || l.get(idx) != answer) {
                         break;
@@ -23,9 +28,11 @@ class Solution {
                     idx += 1;
                 }
             }
+            // answer만큼 인용한 논문의 개수가 (전체 논문 - answer이하만큼 인용한 논문) 이상이면 break
             if (answer >= citations.length - idx) {
                 break;
             }
+            // answer 1 증가
             answer += 1;
         }
         return answer;
