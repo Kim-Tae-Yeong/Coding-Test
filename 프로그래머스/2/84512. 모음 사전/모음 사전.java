e9f976d0e5d51fdaaf7b7254ee8cb5c2d8bfc260
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-11-04 20:56:11 +0900

    Update 모음 사전.java

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -5,6 +5,9 @@ class Solution {
     public int solution(String word) {
         int answer = 0;
         char[] ch = word.toCharArray();
+        // 현재 단어가 A이면 1을 더함
+        // A가 아닌 단어이면서 마지막(5번째) 단어이면 각 단어에 맞게 증가시킴
+        // A가 아닌 단어이면서 마지막(5번쨰) 단어가 아니면 5 + 5 ^ 2 + 5 ^ 3 + ... 으로 더함
         for (int i = 0; i < ch.length; i++) {
             if (ch[i] == 'A') {
                 answer += 1;
@@ -12,6 +15,7 @@ class Solution {
                 if (i == 4) {
                     answer += 2;
                 } else {
+                    // 해당 단어 이후에 아무 단어도 없는 경우를 포함하기 위해서 초기 값을 1로 설정함
                     int tmp = 1;
                     for (int j = 4 - i; j > 0; j--) {
                         tmp += Math.pow(5, j);
