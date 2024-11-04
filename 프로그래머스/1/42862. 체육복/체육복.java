9da73db3775b540b14e90136660558d94e40145f
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-11-04 22:06:23 +0900

    Update 체육복.java

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -5,21 +5,33 @@ class Solution {
     
 
     public int solution(int n, int[] lost, int[] reserve) {
+        // 초기 정답은 전체 인원에서 체육복을 잃어버린 인원의 수
         int answer = n - lost.length;
+        // n명 중 체육복을 잃어버린 인원 확인
         boolean[] still = new boolean[n + 2];
         for (int elem : lost) {
             still[elem] = true;
         }
+        // 여분의 체육복이 있는 인원 중
         List<Integer> l = new ArrayList<>();
         for (int elem : reserve) {
+            // 본인의 체육복을 잃어버린 인원이면
             if (still[elem]) {
+                // 정답 증가
                 answer += 1;
+                // 잃어버린 인원 갱신
                 still[elem] = false;
-            } else {
+            } 
+            // 그게 아니면
+            else {
+                // 배열에 추가
                 l.add(elem);
             }
         }
+        // 배열 정렬
+        // 앞쪽에서 체육복을 잃어버린 인원은 앞쪽의 여분 있는 인원이 빌려줘야 더 많은 인원이 체육복을 가질 수 있음
         l.sort(null);
+        // 이후 앞 뒤로 체육복 빌려줄 인원 확인
         for (int elem : l) {
             if (still[elem - 1]) {
                 answer += 1;
