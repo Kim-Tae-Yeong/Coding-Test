6b0ff37e895d7aa74db417086d19d3638f65bb3b
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-12-04 20:45:26 +0900

    Update 별 찍기 － 11.java

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -7,6 +7,7 @@ public class Main {
   private static char[][] map;
 
   private static void draw(int row, int col, int size) {
+    // 크기가 3이면 현재 위치에서 별 그리기
     if (size == 3) {
       map[row][col] = '*';
       map[row + 1][col - 1] = '*';
@@ -16,10 +17,16 @@ public class Main {
       map[row + 2][col] = '*';
       map[row + 2][col + 1] = '*';
       map[row + 2][col + 2] = '*';
-    } else {
+    }
+    // 크기가 3보다 크면
+    else {
+      // 크기를 절반으로 줄임
       size = size / 2;
+      // 현재 위치에서 함수 실행
       draw(row, col, size);
+      // 좌하단으로 이동 후 함수 실행
       draw(row + size, col - size, size);
+      // 우하단으로 이동 후 함수 실행
       draw(row + size, col + size, size);
     }
   }
@@ -30,12 +37,15 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     n = Integer.parseInt(br.readLine());
+    // row : n & col : 2 * n 배열 생성
     map = new char[n][2 * n];
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < 2 * n; j++) {
+        // 공백으로 초기화
         map[i][j] = ' ';
       }
     }
+    // row = 0 & col = n - 1 위치에서 재귀 함수 실행
     draw(0, n - 1, n);
     for (char[] row : map) {
       System.out.println(row);
