4412d5004816ca80822d665f87004fd8a2230a36
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2024-12-15 16:26:07 +0900

    Update 이진 검색 트리.java

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -2,12 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main_5639 {
   private static class node {
     int num;
     node leftNode;
     node rightNode;
 
+    // tree의 root node 초기화
     node(int num) {
       this.num = num;
       this.leftNode = null;
@@ -23,19 +24,25 @@ public class Main {
     }
 
     private static node insertRecursively(node current, int num) {
+      // tree가 비어있거나 leaf node까지 가면 새로운 node 생성
       if (current == null) {
         return new node(num);
       }
 
+      // 현재 node의 값보다 작으면 왼쪽 node에서 값 비교
       if (num < current.num) {
         current.leftNode = insertRecursively(current.leftNode, num);
-      } else if (num > current.num) {
+      }
+      // 현재 node의 값보다 크면 오른쪽 node에서 값 비교
+      else if (num > current.num) {
         current.rightNode = insertRecursively(current.rightNode, num);
       }
 
+      // 현재 값을 가진 node를 생성 후 반환
       return current;
     }
 
+    // 후위 탐색 : 왼쪽 -> 오른쪽 -> 자신
     private static void postOrder(node current) {
       if (current != null) {
         postOrder(current.leftNode);
@@ -52,6 +59,7 @@ public class Main {
 
     while (true) {
       try {
+        // 숫자를 계속 입력받다가 error가 발생하면 break
         int num = Integer.parseInt(br.readLine());
         binaryTree.insert(num);
       } catch (NumberFormatException e) {
