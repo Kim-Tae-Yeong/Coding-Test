d89ff065515c278b67df5bebc86a8f89822d72ee
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-01-06 18:52:51 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 760 ms, Memory: 148364 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[][] map;
+    private static boolean[][][] visited;
+    private static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    private static class State {
+        int row, col, wall, cnt;
+        public State(int row, int col, int wall, int cnt) {
+            this.row = row;
+            this.col = col;
+            this.wall = wall;
+            this.cnt = cnt;
+        }
+    }
+    
+    private static int bfs() {
+        Queue<State> q = new LinkedList<>();
+        q.add(new State(0, 0, 0, 1));
+        visited[0][0][0] = true;
+
+        while (!q.isEmpty()) {
+            State cur = q.poll();
+            int row = cur.row;
+            int col = cur.col;
+            int wall = cur.wall;
+            int cnt = cur.cnt;
+
+            if (row == N - 1 && col == M - 1) {
+                return cnt;
+            }
+
+            for (int[] d : dir) {
+                int nr = row + d[0];
+                int nc = col + d[1];
+                
+                if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
+                    if (map[nr][nc] == 0 && !visited[nr][nc][wall]) {
+                        visited[nr][nc][wall] = true;
+                        q.add(new State(nr, nc, wall, cnt + 1));
+                    } else if (map[nr][nc] == 1 && wall == 0 && !visited[nr][nc][1]) {
+                        visited[nr][nc][1] = true;
+                        q.add(new State(nr, nc, 1, cnt + 1));
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // BufferedReader br = new BufferedReader(new FileReader("./2206.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] num = br.readLine().split(" ");
+        N = Integer.parseInt(num[0]);
+        M = Integer.parseInt(num[1]);
+        map = new int[N][M];
+        visited = new boolean[N][M][2]; // 벽을 부순 여부를 나타내는 추가 차원
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs());
+        br.close();
+    }
+}
