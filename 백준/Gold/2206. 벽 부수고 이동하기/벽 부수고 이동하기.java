d5a48caac55f84966b0e9053cdc4448bd3284e74
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-01-06 21:12:23 +0900

    Update 벽 부수고 이동하기.java

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,74 +1,81 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    private static int N, M;
-    private static int[][] map;
-    private static boolean[][][] visited;
-    private static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    
-    private static class State {
-        int row, col, wall, cnt;
-        public State(int row, int col, int wall, int cnt) {
-            this.row = row;
-            this.col = col;
-            this.wall = wall;
-            this.cnt = cnt;
-        }
+public class Main_2206 {
+  private static int N, M;
+  private static int[][] map;
+  private static boolean[][][] visited;
+  private static int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+  private static class State {
+    int row, col, wall, cnt;
+
+    public State(int row, int col, int wall, int cnt) {
+      this.row = row;
+      this.col = col;
+      this.wall = wall;
+      this.cnt = cnt;
     }
-    
-    private static int bfs() {
-        Queue<State> q = new LinkedList<>();
-        q.add(new State(0, 0, 0, 1));
-        visited[0][0][0] = true;
+  }
 
-        while (!q.isEmpty()) {
-            State cur = q.poll();
-            int row = cur.row;
-            int col = cur.col;
-            int wall = cur.wall;
-            int cnt = cur.cnt;
+  private static boolean isInBound(int row, int col) {
+    return 0 <= row && row < N && 0 <= col && col < M;
+  }
 
-            if (row == N - 1 && col == M - 1) {
-                return cnt;
-            }
+  private static int bfs() {
+    Queue<State> q = new LinkedList<>();
+    q.add(new State(0, 0, 0, 1));
+    visited[0][0][0] = true;
 
-            for (int[] d : dir) {
-                int nr = row + d[0];
-                int nc = col + d[1];
-                
-                if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
-                    if (map[nr][nc] == 0 && !visited[nr][nc][wall]) {
-                        visited[nr][nc][wall] = true;
-                        q.add(new State(nr, nc, wall, cnt + 1));
-                    } else if (map[nr][nc] == 1 && wall == 0 && !visited[nr][nc][1]) {
-                        visited[nr][nc][1] = true;
-                        q.add(new State(nr, nc, 1, cnt + 1));
-                    }
-                }
-            }
+    while (!q.isEmpty()) {
+      State cur = q.poll();
+      int row = cur.row;
+      int col = cur.col;
+      int wall = cur.wall;
+      int cnt = cur.cnt;
+
+      // bfs는 레벨 단위 탐색을 기반으로 하기 때문에 가장 먼저 도착 지점에 도달하는 경우가 최단거리가 됨
+      if (row == N - 1 && col == M - 1) {
+        return cnt;
+      }
+
+      for (int[] d : dir) {
+        int nr = row + d[0];
+        int nc = col + d[1];
+
+        if (isInBound(nr, nc)) {
+          if (map[nr][nc] == 0 && !visited[nr][nc][wall]) {
+            visited[nr][nc][wall] = true;
+            q.add(new State(nr, nc, wall, cnt + 1));
+          } else if (map[nr][nc] == 1 && wall == 0 && !visited[nr][nc][1]) {
+            visited[nr][nc][1] = true;
+            q.add(new State(nr, nc, 1, cnt + 1));
+          }
         }
-        return -1;
+      }
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) throws Exception {
+    // BufferedReader br = new BufferedReader(new FileReader("./2206.txt"));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    map = new int[N][M];
+    // 벽을 부순 경우와 부수지 않은 상태를 구분하여 방문을 기록함
+    visited = new boolean[N][M][2];
+
+    for (int i = 0; i < N; i++) {
+      String line = br.readLine();
+      for (int j = 0; j < M; j++) {
+        map[i][j] = line.charAt(j) - '0';
+      }
     }
 
-    public static void main(String[] args) throws Exception {
-        // BufferedReader br = new BufferedReader(new FileReader("./2206.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] num = br.readLine().split(" ");
-        N = Integer.parseInt(num[0]);
-        M = Integer.parseInt(num[1]);
-        map = new int[N][M];
-        visited = new boolean[N][M][2]; // 벽을 부순 여부를 나타내는 추가 차원
-
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = line.charAt(j) - '0';
-            }
-        }
-
-        System.out.println(bfs());
-        br.close();
-    }
+    System.out.println(bfs());
+    br.close();
+  }
 }
