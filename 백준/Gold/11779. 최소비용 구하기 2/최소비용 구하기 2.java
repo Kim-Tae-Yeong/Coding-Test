b122e61e947c4f00d58f0ca8124a013d03dcc7f3
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-01-17 20:55:32 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 496 ms, Memory: 51036 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -1,98 +1,96 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-  static int N, M, start, end;
-  static StringTokenizer st;
-  static List<Edge> edges = new ArrayList<>();
-  static int[] dist, path;
-  static PriorityQueue<Edge> pq = new PriorityQueue<>();
-  static Stack<Integer> s = new Stack<>();
+    static int N, M, start, end;
+    static List<List<Edge>> graph;
+    static int[] dist, path;
+    static PriorityQueue<Edge> pq;
+    static Stack<Integer> s = new Stack<>();
 
-  static class Edge implements Comparable<Edge> {
-    int from, to, cost;
+    static class Edge implements Comparable<Edge> {
+        int to, cost;
 
-    public Edge(int from, int to, int cost) {
-      this.from = from;
-      this.to = to;
-      this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Edge o) {
-      return Integer.compare(this.cost, o.cost);
-    }
-  }
-
-  static void dijkstra() {
-    Arrays.fill(dist, Integer.MAX_VALUE);
-    dist[start] = 0;
-
-    for (Edge edge : edges) {
-      if (edge.from == start) {
-        pq.add(edge);
-      }
-    }
-
-    while (!pq.isEmpty()) {
-      Edge edge = pq.poll();
-      if (dist[edge.to] > dist[edge.from] + edge.cost) {
-        path[edge.to] = edge.from;
-        dist[edge.to] = dist[edge.from] + edge.cost;
-        for (Edge nextEdge : edges) {
-          if (nextEdge.from == edge.to && dist[nextEdge.to] > dist[nextEdge.from] + nextEdge.cost) {
-            pq.add(nextEdge);
-          }
+        public Edge(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
         }
-      }
-    }
-  }
 
-  public static void main(String[] args) throws Exception {
-    // BufferedReader br = new BufferedReader(new FileReader("./11779.txt"));
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-
-    st = new StringTokenizer(br.readLine());
-    M = Integer.parseInt(st.nextToken());
-
-    for (int i = 0; i < M; i++) {
-      st = new StringTokenizer(br.readLine());
-      int from = Integer.parseInt(st.nextToken());
-      int to = Integer.parseInt(st.nextToken());
-      int cost = Integer.parseInt(st.nextToken());
-      Edge edge = new Edge(from, to, cost);
-      edges.add(edge);
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.cost, o.cost);  // 비용 오름차순 정렬
+        }
     }
 
-    st = new StringTokenizer(br.readLine());
-    start = Integer.parseInt(st.nextToken());
-    end = Integer.parseInt(st.nextToken());
+    static void dijkstra() {
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        pq.add(new Edge(start, 0));
 
-    dist = new int[N + 1];
-    path = new int[N + 1];
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            int current = edge.to;
+            int currentCost = edge.cost;
 
-    dijkstra();
-    System.out.println(dist[end]);
-    int num = end;
-    while (num != 0) {
-      s.push(num);
-      num = path[num];
+            // 최적 거리보다 크다면 무시
+            if (dist[current] < currentCost) continue;
+
+            for (Edge next : graph.get(current)) {
+                int newCost = dist[current] + next.cost;
+                if (newCost < dist[next.to]) {
+                    dist[next.to] = newCost;
+                    path[next.to] = current;
+                    pq.add(new Edge(next.to, newCost));
+                }
+            }
+        }
     }
-    System.out.println(s.size());
-    while (!s.isEmpty()) {
-      System.out.print(s.pop() + " ");
-    }
 
-    br.close();
-  }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(from).add(new Edge(to, cost));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+
+        dist = new int[N + 1];
+        path = new int[N + 1];
+        pq = new PriorityQueue<>();
+
+        dijkstra();
+
+        System.out.println(dist[end]);
+
+        int num = end;
+        while (num != 0) {
+            s.push(num);
+            num = path[num];
+        }
+        System.out.println(s.size());
+        while (!s.isEmpty()) {
+            System.out.print(s.pop() + " ");
+        }
+
+        br.close();
+    }
 }
