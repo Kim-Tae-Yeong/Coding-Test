985941e7af5d2712a1b9e891aa0c341369cf09dd
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-01-20 19:34:59 +0900

    Update 아기 상어.java

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_16236 {
   static int N;
   static int size = 2, cnt = 0, ans = 0;
   static int minTime = Integer.MAX_VALUE;
@@ -22,16 +22,25 @@ public class Main {
     minFish[1] = -1;
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
+        // 현재 상어 크기보다 작은 물고기를 발견하면
         if (map[i][j] != 0 && map[i][j] < size) {
+          // 해당 물고기까지 걸리는 시간 탐색
           time = reachFish(i, j);
+          // 시간이 -1이면(해당 물고기에 도달하지 못하면) continue
           if (time == -1) {
             continue;
           }
+          // 이전에 기록된 시간보다 더 짧은 시간이 있다면
+          // 즉 더 가까운 거리에 먹을 수 있는 물고기가 있으면
           if (time < minTime) {
+            // 최소 시간 및 물고기 위치 갱신
             minTime = time;
             minFish[0] = i;
             minFish[1] = j;
-          } else if (time == minTime) {
+          }
+          // 같은 시간으로 측정된 물고기가 있으면
+          else if (time == minTime) {
+            // 조건에 맞게 위치 변경
             if (i < minFish[0]) {
               minFish[0] = i;
               minFish[1] = j;
@@ -96,17 +105,25 @@ public class Main {
     }
 
     while (true) {
+      // 먹을 수 있는 물고기가 있는지 & 해당 물고기에 도달할 수 있는지 확인
       int time = findFish();
       if (time == Integer.MAX_VALUE) {
         break;
       }
+      // 해당 물고기까지 걸리는 시간 추가
       ans = ans + time;
+      // 먹은 물고기 수 증가
       cnt++;
+      // 먹은 물고기 수가 현재 물고기 크기랑 같아지면
       if (cnt == size) {
+        // 크기 증가
         size++;
+        // 먹은 물고기 수 초기화
         cnt = 0;
       }
+      // 현재 먹은 물고기 위치 0으로 변경
       map[minFish[0]][minFish[1]] = 0;
+      // 상어 위치 변경
       current[0] = minFish[0];
       current[1] = minFish[1];
     }
