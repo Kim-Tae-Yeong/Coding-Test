5812261ab1ac5ae37933a00299672b6093b23790
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-01-23 17:18:42 +0900

    Update 후위 표기식.java

--- 백준/Gold/1918. 후위 표기식/후위 표기식.java
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -4,8 +4,11 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_1918 {
   static StringBuilder sb = new StringBuilder();
+  // 우선순위 큐에 Info 클래스를 저장할 때
+  // 1. 우선순위는 내림차순 정렬(우선순위가 높은 연산이 큐의 앞쪽에 위치)
+  // 2. 우선순위가 같은 경우 들어온 시간은 오름차순 정렬(먼저 들어온 연산이 앞쪽에 위치)
   static PriorityQueue<Info> pq = new PriorityQueue<>((o1, o2) -> {
     if (o1.priority == o2.priority) {
       return Integer.compare(o1.time, o2.time);
@@ -14,6 +17,7 @@ public class Main {
     }
   });
 
+  // (연산 기호, 우선순위, 들어온 시간)을 Info 클래스로 정의
   static class Info {
     Character cal;
     int priority;
@@ -26,6 +30,23 @@ public class Main {
     }
   }
 
+  // 현재 연산 기호보다 우선순위가 높거나 같은 우선순위에 먼저 들어온 연산 기호를 뺌
+  static void addCal(int priority, int time) {
+    while (true) {
+      if (pq.isEmpty()) {
+        break;
+      }
+      Info info = pq.peek();
+      if (info.priority > priority) {
+        sb.append(pq.poll().cal);
+      } else if (info.priority == priority && info.time < time) {
+        sb.append(pq.poll().cal);
+      } else {
+        break;
+      }
+    }
+  }
+
   public static void main(String[] args) throws Exception {
     // BufferedReader br = new BufferedReader(new FileReader("./1918.txt"));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,51 +56,38 @@ public class Main {
     int priority = 0;
     for (int i = 0; i < input.length(); i++) {
       Character c = input.charAt(i);
+      // 문자가 괄호면 우선순위를 2 증가시킴
+      // * or / 두 연산 기호에서 우선순위를 1 증가시키는데 괄호는 이것보다 더 높은 우선순위를 가지기 때문
       if (c == '(') {
         priority = priority + 2;
-      } else if (c == ')') {
+      }
+      // 괄호가 닫히면 증가시킨 우선순위를 원래대로 되돌림
+      else if (c == ')') {
         priority = priority - 2;
-      } else if (c == '*' || c == '/') {
+      }
+      // * or / 연산 기호는 이전 연산기호보다 먼저 연산되기 때문에 우선순위를 1 증가시킴
+      else if (c == '*' || c == '/') {
         priority++;
-        while (true) {
-          if (pq.isEmpty()) {
-            break;
-          }
-          Info info = pq.peek();
-          if (info.priority > priority) {
-            sb.append(pq.poll().cal);
-          } else if (info.priority == priority && info.time < i) {
-            sb.append(pq.poll().cal);
-          } else {
-            break;
-          }
-        }
+        // 증가시킨 우선순위보다 높은 우선순위가 있다면 정답으로 옮김
+        addCal(priority, i);
+        // 큐에 현재 정보 저장
         pq.add(new Info(c, priority, i));
+        // 우선순위를 원래대로 되돌림
         priority--;
-      } else if (c == '+' || c == '-') {
+      }
+      // + or - 연산 기호는 이전 연산 기호와 동일한 우선순위를 가짐
+      // 떄문에 우선순위를 변경할 필요가 없음
+      else if (c == '+' || c == '-') {
+        addCal(priority, i);
         pq.add(new Info(c, priority, i));
-        while (true) {
-          if (pq.isEmpty()) {
-            break;
-          }
-          Info info = pq.peek();
-          if (info.priority > priority) {
-            sb.append(pq.poll().cal);
-          } else if (info.priority == priority && info.time < i) {
-            sb.append(pq.poll().cal);
-          } else {
-            break;
-          }
-        }
-      } else {
+      }
+      // 연산 기호가 아닌 피연산자(알파벳)이면 그냥 정답에 저장
+      else {
         sb.append(c);
       }
-      // for (Info info : pq) {
-      // System.out.print(info.cal);
-      // }
-      // System.out.println();
     }
 
+    // 큐에 남아있는 연산 기호를 정답에 옮김
     while (!pq.isEmpty()) {
       sb.append(pq.poll().cal);
     }
