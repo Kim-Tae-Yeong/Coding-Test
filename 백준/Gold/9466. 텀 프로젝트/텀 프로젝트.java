3d6267907a2c5f64871f362fcd1f0825a5acb7ae
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-05 19:15:05 +0900

    Update 텀 프로젝트.java

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -7,15 +7,18 @@ import java.util.Deque;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_9466 {
   static int t, n, ans;
   static int[] num;
   static boolean[] visited;
   static Deque<Integer> d = new ArrayDeque<>();
 
   static void dfs(int current) {
+    // 현재 인원을 deque에 넣음
     d.add(current);
+    // 방문 여부 갱신
     visited[current] = true;
+    // 현재 인원이 선택한 인원을 방문하지 않은 경우 dfs 실행
     int next = num[current];
     if (!visited[next]) {
       dfs(next);
@@ -43,18 +46,25 @@ public class Main {
       ans = 0;
 
       for (int j = 1; j < n + 1; j++) {
+        // 방문하지 않은 인원의 경우
         if (!visited[j]) {
+          // 해당 인원에서 dfs를 시작하면서 deque에 인원을 넣음
           d = new ArrayDeque<>();
           dfs(j);
+          // deque의 마지막 인원이 선택한 학생을 기록
           int start = num[d.peekLast()];
           while (true) {
+            // deque의 모든 인원을 확인하면 break
             if (d.isEmpty()) {
               break;
             }
+            // deque의 앞부터 인원을 확인하면서 위에서 기록한 인원이 deque에 있는지(사이클이 있는지) 확인
             int end = d.pollFirst();
+            // 사이클이 발견되면 break
             if (start == end) {
               break;
             }
+            // 사이클이 발견되기 전 인원은 팀이 없는 인원이기 때문에 정답을 하나씩 늘림
             ans++;
           }
         }
