3f93c8c9b80212dd792adf81e8d30e80823149a2
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-10 16:10:10 +0900

    [Gold IV] Title: 공유기 설치, Time: 332 ms, Memory: 38552 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -4,10 +4,11 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main_2110 {
+public class Main {
   // left : 인접한 공유기 사이의 최소 거리
   // right : 인접한 공유기 사이의 최대 거리
   static int N, C, left, right;
+  static int ans = 0;
   static int[] pos;
 
   static int getCount(long size) {
@@ -19,7 +20,7 @@ public class Main_2110 {
     for (int i = 1; i < N; i++) {
       // 각 집과 이전에 공유기를 설치한 집과의 거리를 구함
       // 두 집 사이의 거리가 size보다 크면 현재 집에 공유기를 설치해야함
-      if (pos[i] - lastPos > size) {
+      if (pos[i] - lastPos >= size) {
         count++;
         lastPos = pos[i];
       }
@@ -50,22 +51,23 @@ public class Main_2110 {
     // 최대 거리는 첫 번째 집에서 가장 멀리 떨어진 집과의 거리로 설정
     right = pos[N - 1] - pos[0];
 
-    while (left < right) {
+    while (left <= right) {
       // 중간 거리를 구함
       int mid = (left + right) / 2;
       // 해당 거리로 공유기를 설치할 때 필요한 개수를 구함
       int count = getCount(mid);
       // 현재 거리를 기준으로 설치한 공유기가 C보다 많거나 같으면 최소 거리를 늘림
       if (count >= C) {
+        ans = mid;
         left = mid + 1;
       }
       // 공유기가 C보다 적으면 최대 거리를 줄임
       else {
-        right = mid;
+        right = mid - 1;
       }
     }
 
-    System.out.println(left);
+    System.out.println(ans);
 
     br.close();
   }
