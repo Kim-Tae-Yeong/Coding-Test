4502b86842e6a80b46750c46033a9e3c4ea300d9
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-10 20:10:56 +0900

    Update 서울에 위치한 식당 목록 출력하기.sql

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,3 +1,6 @@
+-- REST_INFO as i inner join REST_REVIEW as r on i.REST_ID = r.REST_ID : 두 table을 REST_ID 기준으로 join
+-- like '서울%' : 서울로 시작하는 경우
+-- group by i.REST_ID : REST_ID마다 한 줄씩 결과 출력(group by를 사용하지 않으면 전체 평균 하나만 반환)
 select i.REST_ID, i.REST_NAME, i.FOOD_TYPE, i.FAVORITES, i.ADDRESS, round(avg(r.REVIEW_SCORE), 2) as SCORE
 from REST_INFO as i inner join REST_REVIEW as r on i.REST_ID = r.REST_ID
 where ADDRESS like "서울%"
