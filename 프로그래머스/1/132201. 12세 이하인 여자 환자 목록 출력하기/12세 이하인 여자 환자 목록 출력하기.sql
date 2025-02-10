e6b9a3a8731aa2900fed6b44ee32ae34f64aa1ee
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-10 20:43:48 +0900

    Update 12세 이하인 여자 환자 목록 출력하기.sql

--- 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -1,3 +1,4 @@
+-- COALESCE(tlno, 'NONE') : tlno가 null이면 'NONE' 반환(아니면 tlno 그대로 반환)
 select pt_name, pt_no, gend_cd, age, COALESCE(tlno, 'NONE') as tlno
 from patient
 where age <= 12 and gend_cd = 'W'
