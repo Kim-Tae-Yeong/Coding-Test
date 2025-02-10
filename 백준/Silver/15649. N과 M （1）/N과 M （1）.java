fa94d32f3dc7e868c29833669c5c9761f3d47f23
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-10 14:13:28 +0900

    Update N과 M （1）.java

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,12 +3,14 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_15649 {
   static int N, M;
+  // visited[i] : 현재 경우에 i가 포함됨
   static boolean[] visited;
   static int[] ans;
 
   static void backTracking(int cnt) {
+    // M개를 모두 고르면 출력
     if (cnt == M) {
       for (int num : ans) {
         System.out.print(num + " ");
@@ -16,11 +18,16 @@ public class Main {
       System.out.println();
       return;
     }
+    // 현재 경우에서 1부터 N까지 숫자 탐색
     for (int i = 1; i < N + 1; i++) {
+      // i가 현재 경우에 포함되지 않은 수이면
       if (!visited[i]) {
+        // 현재 경우에 i를 넣음
         visited[i] = true;
         ans[cnt] = i;
+        // 다시 백트래킹 실시
         backTracking(cnt + 1);
+        // i가 포함된 모든 경우 탐색이 끝나면 i를 현재 경우에서 제외시킴
         visited[i] = false;
       }
     }
@@ -37,6 +44,7 @@ public class Main {
     visited = new boolean[N + 1];
     ans = new int[M];
 
+    // M개를 선택할 때까지 백트래킹 실시
     backTracking(0);
 
     br.close();
