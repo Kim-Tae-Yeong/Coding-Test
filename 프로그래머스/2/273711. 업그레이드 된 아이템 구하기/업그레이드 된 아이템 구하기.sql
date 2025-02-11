fab937f50abc6d947918883085beb19f55677060
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-11 22:59:57 +0900

    Update 업그레이드 된 아이템 구하기.sql

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,3 +1,6 @@
+-- select item_id from item_info where rarity = 'rare' : rare인 itme의 id 찾기
+-- select item_id from item_tree where parent_item_id in ~ : rare인 item이 업그레이드 가능한 item의 id 찾기
+-- select item_id, item_name, rarity from item_info where item_id in ~ : rare인 item이 업그레이드 가능한 item의 id로 해당 item의 정보 찾기
 select item_id, item_name, rarity
 from item_info
 where item_id in (
