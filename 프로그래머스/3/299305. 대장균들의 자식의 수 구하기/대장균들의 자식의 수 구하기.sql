b7696b37b689d6688cab3e0e3d13e5b475f2bdc3
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-12 16:27:34 +0900

    Update 대장균들의 자식의 수 구하기.sql

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -1,3 +1,17 @@
 select id, (select count(*) from ecoli_data where parent_id = e.id) as child_count
 from ecoli_data as e
 order by id;
+
+-- SELECT 
+--     parent.ID, 
+--     COUNT(child.ID) AS CHILD_COUNT
+-- FROM 
+--     ECOLI_DATA AS parent
+-- LEFT JOIN 
+--     ECOLI_DATA AS child
+-- ON 
+--     parent.ID = child.PARENT_ID
+-- GROUP BY 
+--     parent.ID
+-- ORDER BY 
+--     parent.ID;
