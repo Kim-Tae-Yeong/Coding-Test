0e4387311600e33fda91278350415ce913e4bacf
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-13 16:15:43 +0900

    Update 거스름돈.java

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main_5585 {
   static int money, remain;
   static int ans = 0;
   static int[] rests = { 500, 100, 50, 10, 5, 1 };
@@ -14,6 +14,9 @@ public class Main {
     money = Integer.parseInt(st.nextToken());
     remain = 1000 - money;
 
+    // 600엔을 (100엔 * 6개)로 줄 수 있지만 (500엔 + 100엔)으로도 줄 수 있음
+    // 즉 현재 거스름돈을 가장 큰 단위의 잔돈으로 나눔
+    // 이후 나머지를 다시 다음 큰 단위의 동전으로 나누는 방식으로 최소 개수를 구함
     for (int rest : rests) {
       ans += (remain / rest);
       remain = remain % rest;
