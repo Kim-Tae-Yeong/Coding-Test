7370289ba904c8dedb5bbc684ad00ef569ffc9e4
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-14 16:23:11 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -0,0 +1,11 @@
+select
+    hour(datetime) as hour,
+    count(*) as count
+from
+    animal_outs
+where
+    hour(datetime) >= 9 and hour(datetime) <= 19
+group by
+    hour(datetime)
+order by
+    hour(datetime);
