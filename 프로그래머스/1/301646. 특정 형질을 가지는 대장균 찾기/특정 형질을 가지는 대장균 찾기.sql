0de9ee180d92ce1736cb0c9839cf3fd7fc5e6fa0
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-17 17:27:08 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,3 +1,7 @@
-select count(*) as count
-from ecoli_data
-where genotype & 2 != 2 and (genotype & 1 = 1 or genotype & 4 = 4);
+select
+    count(*) as count
+from
+    ecoli_data
+where
+    genotype & 2 != 2 and
+    (genotype & 1 = 1 or genotype & 4 = 4)
