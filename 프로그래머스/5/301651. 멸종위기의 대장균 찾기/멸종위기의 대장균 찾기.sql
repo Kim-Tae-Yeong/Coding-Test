be22f06e835e940e5ebfd0ea9aa2a88f69910181
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-17 17:48:09 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,21 +1,33 @@
-with recursive ecoli_tree as (
-    select id, parent_id, 1 as generation
-    from ecoli_data
-    where parent_id is null
-    
+with recursive family as (
+    select
+        id,
+        parent_id,
+        1 as generation
+    from
+        ecoli_data
+    where
+        parent_id is null
     union all
-    
-    select ed.id, ed.parent_id, et.generation + 1
-    from ecoli_data as ed
-    join ecoli_tree as et on ed.parent_id = et.id
+    select
+        t1.id,
+        t1.parent_id,
+        t2.generation + 1
+    from
+        ecoli_data as t1
+        join family as t2
+        on t1.parent_id = t2.id   
 )
 
-select count(*) as count, generation
-from ecoli_tree
-where id not in (
-    select distinct parent_id
-    from ecoli_data
-    where parent_id is not null
-)
-group by generation
-order by generation;
+select
+    count(distinct t1.id) as count,
+    generation
+from
+    family as t1
+    left join ecoli_data as t2
+    on t1.id = t2.parent_id
+where
+    t2.id is null
+group by
+    generation
+order by
+    2
