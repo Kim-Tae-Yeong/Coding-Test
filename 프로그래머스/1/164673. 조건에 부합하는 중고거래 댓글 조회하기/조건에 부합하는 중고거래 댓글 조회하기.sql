8ae7f2bdd3f43d4fe62502c4b4a57eea2ea6ac91
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-17 11:50:41 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,4 +1,17 @@
-select b.title, b.board_id, r.reply_id, r.writer_id, r.contents, date_format(r.created_date, '%Y-%m-%d') as created_date
-from used_goods_board as b inner join used_goods_reply as r on b.board_id = r.board_id
-where date_format(b.created_date, '%Y-%m') = '2022-10'
-order by r.created_date, b.title;
+select
+    b.title,
+    b.board_id,
+    r.reply_id,
+    r.writer_id,
+    r.contents,
+    date_format(r.created_date, '%Y-%m-%d') as created_date
+from
+    used_goods_board as b
+    join used_goods_reply as r
+    on b.board_id = r.board_id
+where
+    b.created_date like '2022-10%'
+order by
+    r.created_date,
+    b.title
+    
