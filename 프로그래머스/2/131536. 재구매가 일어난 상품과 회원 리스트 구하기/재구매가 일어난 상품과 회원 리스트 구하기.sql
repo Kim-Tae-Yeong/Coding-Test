f8239e2aabde053828e2516a158ad26b65a0e6cb
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-17 12:01:18 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,8 +1,13 @@
--- user_id와 product_id로 그룹을 나눔
--- 이렇게만 하면 해당 user_id와 product_id가 몇 번 나왔는지 알 수 없음
--- 때문에 having count(*) >= 2를 사용해 2번 이상 나온 행만 출력
-select user_id, product_id
-from online_sale
-group by user_id, product_id
-having count(*) >= 2
-order by user_id, product_id desc;
+select
+    user_id,
+    product_id
+from
+    online_sale
+group by
+    user_id,
+    product_id
+having
+    count(*) >= 2
+order by
+    user_id,
+    product_id desc
