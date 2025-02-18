8833cc97f635fb184545f1970eb0b91e0ab838dc
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-18 16:12:12 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,9 +1,27 @@
-select f.id, n.fish_name, f.length
-from fish_info as f
+with max_fish as (
+    select
+        fish_type,
+        max(length) as length
+    from
+        fish_info
+    group by
+        fish_type
+)
+
+select
+    i.id,
+    n.fish_name,
+    i.length
+from
+    fish_info as i
     join fish_name_info as n
-    on f.fish_type = n.fish_type
-where f.length = (
-    select max(f2.length)
-    from fish_info as f2
-    where f2.fish_type = f.fish_type
-);
+    on i.fish_type = n.fish_type
+where
+    (i.fish_type, i.length) in (
+        select
+            *
+        from
+            max_fish
+    )
+order by
+    1
