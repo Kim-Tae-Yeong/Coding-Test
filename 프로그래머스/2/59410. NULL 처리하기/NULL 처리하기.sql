503922aa5a25a2123e5a6c2967f68e050d0efbf4
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-18 16:25:12 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,7 +1,8 @@
-SELECT ANIMAL_TYPE
-       ,CASE 
-            WHEN NAME IS NULL THEN 'No name'
-            ELSE NAME END AS NAME
-       ,SEX_UPON_INTAKE
-FROM ANIMAL_INS
-ORDER BY ANIMAL_ID;
+select
+    animal_type,
+    coalesce(name, 'No name') as name,
+    sex_upon_intake
+from
+    animal_ins
+order by
+    animal_id
