75b2a3d19bd30ec258226ff5afd8663c3092b237
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-20 03:10:02 +0900

    Update 피보나치 수.java

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,24 +1,27 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main_2747 {
   static int n;
 
-  // static int fibonacci(int n) {
-  // if (n == 1 || n == 2) {
-  // return 1;
-  // } else {
-  // return (fibonacci(n - 1) + fibonacci(n - 2));
-  // }
-  // }
+  // 피보나치 수를 재귀로 구하면 시간 초과
+  static int fibonacci_recursive(int n) {
+    if (n == 1 || n == 2) {
+      return 1;
+    } else {
+      return (fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2));
+    }
+  }
 
-  static int fibonacci(int n) {
+  static int fibonacci_dp(int n) {
+    // 배열에 f(1)과 f(2) 값을 먼저 기록
     int[] num = new int[n + 1];
     num[0] = 0;
     num[1] = 1;
     if (n >= 2) {
       num[2] = 1;
     }
+    // f(3)부터는 배열에 저장된 값을 이용해서 한번에 계산 & 배열에 기록
     for (int i = 3; i < n + 1; i++) {
       num[i] = num[i - 1] + num[i - 2];
     }
@@ -32,7 +35,7 @@ public class Main {
     StringTokenizer st = new StringTokenizer(br.readLine());
     n = Integer.parseInt(st.nextToken());
 
-    System.out.println(fibonacci(n));
+    System.out.println(fibonacci_dp(n));
     br.close();
   }
 }
