85eb5e92cd395c12ede5a9dd415c8665e1120308
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-19 13:03:42 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -2,15 +2,15 @@ select
     e.emp_no,
     e.emp_name,
     case
-        when a.score / 2 >= 96 then 'S'
-        when a.score / 2 >= 90 then 'A'
-        when a.score / 2 >= 80 then 'B'
+        when g.score / 2 >= 96 then 'S'
+        when g.score / 2 >= 90 then 'A'
+        when g.score / 2 >= 80 then 'B'
         else 'C'
     end as grade,
     case
-        when a.score / 2 >= 96 then sal * 0.2
-        when a.score / 2 >= 90 then sal * 0.15
-        when a.score / 2 >= 80 then sal * 0.1
+        when g.score / 2 >= 96 then e.sal * 0.2
+        when g.score / 2 >= 90 then e.sal * 0.15
+        when g.score / 2 >= 80 then e.sal * 0.1
         else 0
     end as bonus
 from
@@ -19,11 +19,11 @@ from
         select
             emp_no,
             sum(score) as score
-        from 
+        from
             hr_grade
         group by
             emp_no
-    ) as a
-    on e.emp_no = a.emp_no
+    ) as g
+    on e.emp_no = g.emp_no
 order by
-    e.emp_no;
+    1
