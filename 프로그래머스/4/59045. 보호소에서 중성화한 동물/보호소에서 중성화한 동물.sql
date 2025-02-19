ab3a136a7d31b80e8b892da54b81d62713a3efac
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-19 22:06:22 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -7,7 +7,7 @@ from
     join animal_outs as o
     on i.animal_id = o.animal_id
 where
-    i.sex_upon_intake like 'intact%'
-    and (o.sex_upon_outcome like 'spayed%' or o.sex_upon_outcome like 'neutered%')
+    i.sex_upon_intake like 'Intact%' and
+    (o.sex_upon_outcome like 'Spayed%' or o.sex_upon_outcome like 'Neutered%')
 order by
-    i.animal_id
+    1
