9b52b01fbfc8872dbe1fb39168faf880fe105008
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-21 17:02:07 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,19 +1,19 @@
-select
-    a.apnt_no,
-    p.pt_name,
-    a.pt_no,
-    d.mcdp_cd,
-    d.dr_name,
-    a.apnt_ymd
-from
-    appointment as a
-    join patient as p
-    on a.pt_no = p.pt_no
-    join doctor as d
-    on a.mddr_id = d.dr_id
-where
-    d.mcdp_cd = 'cs'
-    and a.apnt_ymd like '2022-04-13%'
-    and a.apnt_cncl_yn = 'n'
-order by
-    a.apnt_ymd
+SELECT
+    A.APNT_NO,
+    P.PT_NAME,
+    P.PT_NO,
+    D.MCDP_CD,
+    D.DR_NAME,
+    A.APNT_YMD
+FROM
+    APPOINTMENT AS A
+    JOIN PATIENT AS P
+    ON A.PT_NO = P.PT_NO
+    JOIN DOCTOR AS D
+    ON A.MDDR_ID = D.DR_ID
+WHERE
+    A.MCDP_CD = 'CS' AND
+    A.APNT_YMD LIKE '2022-04-13%' AND
+    APNT_CNCL_YN = 'N'
+ORDER BY
+    6
