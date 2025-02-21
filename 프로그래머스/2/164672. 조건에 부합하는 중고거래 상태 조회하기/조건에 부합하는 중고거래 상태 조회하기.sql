a6acb78078dc0181ed33aea4be79df39d91eb690
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-21 16:26:23 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,16 +1,16 @@
-select
-    board_id,
-    writer_id,
-    title,
-    price,
-    case
-        when status = 'sale' then '판매중'
-        when status = 'reserved' then '예약중'
-        when status = 'done' then '거래완료'
-    end as status
-from
-    used_goods_board
-where
-    date_format(created_date, '%Y-%m-%d') = '2022-10-05'
-order by
-    board_id desc
+SELECT
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    CASE
+        WHEN STATUS = 'SALE' THEN '판매중'
+        WHEN STATUS = 'RESERVED' THEN '예약중'
+        WHEN STATUS = 'DONE' THEN '거래완료'
+    END AS STATUS
+FROM
+    USED_GOODS_BOARD
+WHERE
+    CREATED_DATE = '2022-10-05'
+ORDER BY
+    1 DESC
