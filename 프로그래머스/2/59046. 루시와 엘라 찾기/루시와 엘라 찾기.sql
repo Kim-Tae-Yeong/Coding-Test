9cb6a1efdcfc072ad86e95dfa1019fe0fe331179
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-21 17:03:11 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,17 +1,17 @@
-select
-    animal_id,
-    name,
-    sex_upon_intake
-from
-    animal_ins
-where
-    name in (
-        'lucy',
-        'ella',
-        'pickle',
-        'rogan',
-        'sabrina',
-        'mitty'
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
+FROM
+    ANIMAL_INS
+WHERE
+    NAME IN (
+        'LUCY',
+        'ELLA',
+        'PICKLE',
+        'ROGAN',
+        'SABRINA',
+        'MITTY'
     )
-order by
-    animal_id
+ORDER BY
+    1
