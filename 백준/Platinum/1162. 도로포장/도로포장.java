012331c8bc4bbb8352afde4cf0dc3054076f8ca1
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-26 22:56:57 +0900

    [Platinum V] Title: 도로포장, Time: 612 ms, Memory: 65280 KB -BaekjoonHub

--- 백준/Platinum/1162. 도로포장/도로포장.java
+++ 백준/Platinum/1162. 도로포장/도로포장.java
@@ -2,85 +2,82 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, K;
-    static List<List<Edge>> graph = new ArrayList<>();
-    static PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingLong(o -> o.cost));
-    static long[][] distance;
-    static long ans = Long.MAX_VALUE;
+  static int N, M, K;
+  static List<List<Edge>> graph = new ArrayList<>();
+  static PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> Long.compare(o1.cost, o2.cost));
+  static long[][] distance;
+  static long ans = Long.MAX_VALUE;
 
-    static void dijkstra() {
-        pq.add(new Edge(1, 0, 0));
+  static void dijkstra() {
+    pq.add(new Edge(1, 0, 0));
 
-        while (!pq.isEmpty()) {
-            Edge e = pq.poll();
+    while (!pq.isEmpty()) {
+      Edge e = pq.poll();
 
-            if (e.cost > distance[e.remove][e.to]) continue;  // 기존 거리보다 크면 무시
+      if (e.cost > distance[e.remove][e.to]) {
+        continue;
+      }
 
-            for (Edge next : graph.get(e.to)) {
-                long newCost = distance[e.remove][e.to] + next.cost;
-
-                // 도로를 그대로 이용하는 경우
-                if (distance[e.remove][next.to] > newCost) {
-                    distance[e.remove][next.to] = newCost;
-                    pq.add(new Edge(next.to, newCost, e.remove));
-                }
-
-                // 도로를 없애는 경우 (최대 K번 가능)
-                if (e.remove < K && distance[e.remove + 1][next.to] > distance[e.remove][e.to]) {
-                    distance[e.remove + 1][next.to] = distance[e.remove][e.to];
-                    pq.add(new Edge(next.to, distance[e.remove + 1][next.to], e.remove + 1));
-                }
-            }
+      for (Edge next : graph.get(e.to)) {
+        if (distance[e.remove][next.to] > distance[e.remove][e.to] + next.cost) {
+          distance[e.remove][next.to] = distance[e.remove][e.to] + next.cost;
+          pq.add(new Edge(next.to, distance[e.remove][next.to], e.remove));
         }
+        if (e.remove < K && distance[e.remove + 1][next.to] > e.cost) {
+          distance[e.remove + 1][next.to] = e.cost;
+          pq.add(new Edge(next.to, e.cost, e.remove + 1));
+        }
+      }
+    }
+  }
+
+  static class Edge {
+    int to;
+    long cost;
+    int remove;
+
+    public Edge(int to, long cost, int remove) {
+      this.to = to;
+      this.cost = cost;
+      this.remove = remove;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    // BufferedReader br = new BufferedReader(new FileReader("./1162.txt"));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+
+    for (int i = 0; i < N + 1; i++) {
+      graph.add(new ArrayList<>());
     }
 
-    static class Edge {
-        int to;
-        long cost;
-        int remove;
-
-        public Edge(int to, long cost, int remove) {
-            this.to = to;
-            this.cost = cost;
-            this.remove = remove;
-        }
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int from = Integer.parseInt(st.nextToken());
+      int to = Integer.parseInt(st.nextToken());
+      int cost = Integer.parseInt(st.nextToken());
+      graph.get(from).add(new Edge(to, cost, 0));
+      graph.get(to).add(new Edge(from, cost, 0));
     }
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        graph = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            graph.get(from).add(new Edge(to, cost, 0));
-            graph.get(to).add(new Edge(from, cost, 0));
-        }
-
-        distance = new long[K + 1][N + 1];
-        for (int i = 0; i <= K; i++) {
-            Arrays.fill(distance[i], Long.MAX_VALUE);
-        }
-        distance[0][1] = 0;  // 출발점 초기화
-
-        dijkstra();
-
-        for (int i = 0; i <= K; i++) {
-            ans = Math.min(ans, distance[i][N]);
-        }
-
-        System.out.println(ans);
-        br.close();
+    distance = new long[K + 1][N + 1];
+    for (int i = 0; i < K + 1; i++) {
+      Arrays.fill(distance[i], Long.MAX_VALUE);
+      distance[i][1] = 0;
     }
+    dijkstra();
+
+    for (int i = 0; i < K + 1; i++) {
+      ans = Math.min(ans, distance[i][N]);
+    }
+
+    System.out.println(ans);
+
+    br.close();
+  }
 }
