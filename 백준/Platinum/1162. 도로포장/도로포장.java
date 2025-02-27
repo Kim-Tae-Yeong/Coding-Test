078b5a749a4b75e471e74d85fe4b0c35b7cc94ef
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-27 12:54:36 +0900

    Update 도로포장.java

--- 백준/Platinum/1162. 도로포장/도로포장.java
+++ 백준/Platinum/1162. 도로포장/도로포장.java
@@ -1,10 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main_1162 {
   static int N, M, K;
+  // graph.get(i) : i번 노드에서 시작하는 간선 정보 저장
   static List<List<Edge>> graph = new ArrayList<>();
+  // 우선순위 큐에 도착 노드까지 거리가 짧은 순으로 정렬
   static PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> Long.compare(o1.cost, o2.cost));
+  // distance[i][j] : i개 도로를 포장했을 때 j번 노드까지 걸리는 최소 거리
   static long[][] distance;
   static long ans = Long.MAX_VALUE;
 
@@ -14,18 +17,24 @@ public class Main {
     while (!pq.isEmpty()) {
       Edge e = pq.poll();
 
+      // 현재 노드에서 다음 노드로 이동하는 거리가 이전에 저장된 거리보다 길면 continue
       if (e.cost > distance[e.remove][e.to]) {
         continue;
       }
 
+      // 현재 노드에서 출발하는 간선 정보 확인
       for (Edge next : graph.get(e.to)) {
+        // 다음 노드로 이동할 때 해당 도로를 포장하지 않는 경우
         if (distance[e.remove][next.to] > distance[e.remove][e.to] + next.cost) {
           distance[e.remove][next.to] = distance[e.remove][e.to] + next.cost;
           pq.add(new Edge(next.to, distance[e.remove][next.to], e.remove));
         }
-        if (e.remove < K && distance[e.remove + 1][next.to] > e.cost) {
-          distance[e.remove + 1][next.to] = e.cost;
-          pq.add(new Edge(next.to, e.cost, e.remove + 1));
+        // 다음 노드로 이동할 때 해당 도로를 포장하는 경우
+        // 도로를 포장하면 해당 거리는 0이 되므로 다음 노드까지의 거리는 현재 노드까지의 거리와 같아짐
+        // 우선순위 큐에는 포장한 도로 개수를 1 증가시켜서 저장
+        if (e.remove < K && distance[e.remove + 1][next.to] > distance[e.remove][e.to]) {
+          distance[e.remove + 1][next.to] = distance[e.remove][e.to];
+          pq.add(new Edge(next.to, distance[e.remove + 1][next.to], e.remove + 1));
         }
       }
     }
@@ -34,6 +43,7 @@ public class Main {
   static class Edge {
     int to;
     long cost;
+    // 현재 노드까지 도달할 때 포장한 도로 개수
     int remove;
 
     public Edge(int to, long cost, int remove) {
@@ -67,7 +77,9 @@ public class Main {
 
     distance = new long[K + 1][N + 1];
     for (int i = 0; i < K + 1; i++) {
+      // 각 도로까지 거리를 최댓값으로 설정
       Arrays.fill(distance[i], Long.MAX_VALUE);
+      // 시작 노드까지의 거리는 0으로 설정
       distance[i][1] = 0;
     }
     dijkstra();
