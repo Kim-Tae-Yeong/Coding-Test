d8f8165f8e30def1c6e3a3a71d16ddca8a9ec0ac
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-02-28 23:21:58 +0900

    Update 보석 도둑.java

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main_1202 {
   static int N, K;
+  // 보석 정보 저장
   static List<int[]> jewels = new ArrayList<>();
   static int[] bags;
+  // 현재 가방 무게까지 담을 수 있는 보석을 가치 기준 내림차순 정렬
   static PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
   static long ans = 0;
 
@@ -30,15 +32,26 @@ public class Main {
       bags[i] = C;
     }
 
+    // 보석을 무게 기준 오름차순 정렬
     jewels.sort((o1, o2) -> Integer.compare(o1[0], o2[0]));
+    // 가방도 오름차순 정렬
+    // 가방의 무게가 가벼운 가방부터 그 가방에 넣을 수 있는 최대 가치의 보석을 넣어야 함
     Arrays.sort(bags);
 
+    // 현재 가방 무게에 담을 수 있는 보석들은 가방의 무게가 커지면 똑같이 담을 수 있음
+    // 즉, 가방의 무게가 커지면 직전 가방에는 담지 못하지만 현재 가방에는 담을 수 있는 보석들만 우선순위 큐에 넣으면 됨
+    // ex) 가방 무게 5 : (1, 2, 3) -> 가방 무게 10 : (1, 2, 3)(중복) + (6, 7, 9)
     int idx = 0;
+    // 가방의 무게를 하나씩 확인
     for (int i = 0; i < K; i++) {
+      // 현재 가방의 무게까지 넣을 수 있는 모든 보석을 우선순위 큐에 집어넣음
       while (idx < N && jewels.get(idx)[0] <= bags[i]) {
         pq.offer(jewels.get(idx)[1]);
         idx++;
       }
+      // 우선순위 큐가 비어있지 않으면
+      // 즉, 가방에 넣을 수 있는 보석이 있다면
+      // 가치가 가장 큰 보석을 가방에 넣음
       if (!pq.isEmpty()) {
         ans += pq.poll();
       }
