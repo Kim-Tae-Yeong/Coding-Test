d9ab7047349c0feac9eb18015719a678ae02eb56
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-01 23:47:10 +0900

    Update 트리.java

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,21 +1,27 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main_1068 {
   static int N, del, root, ans;
   static int[] parents;
 
   static void dfs(int current) {
+    // leaf 노드인지 확인
     boolean leaf = true;
     for (int i = 0; i < N; i++) {
+      // 탐색할 노드가 제거한 노드면 continue
       if (i == del) {
         continue;
       }
+      // 탐색할 노드의 부모 노드가 현재 노드이면
       if (parents[i] == current) {
+        // leaf 노드가 아님
         leaf = false;
+        // 해당 노드에서 dfs 실행
         dfs(i);
       }
     }
+    // leaf 노드이면 개수 증가
     if (leaf) {
       ans++;
     }
@@ -32,6 +38,7 @@ public class Main {
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
       parents[i] = Integer.parseInt(st.nextToken());
+      // root 노드 확인
       if (parents[i] == -1) {
         root = i;
       }
@@ -41,6 +48,7 @@ public class Main {
     del = Integer.parseInt(st.nextToken());
 
     ans = 0;
+    // 제거할 노드가 루트 노드가 아닌 경우에만 dfs 실행
     if (root != del) {
       dfs(root);
     }
