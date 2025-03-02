129943fdc0260a82b9850e57e7b62663fcb2f264
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-03 01:04:41 +0900

    Update 내리막 길.java

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,9 +1,10 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main_1520 {
   static int M, N;
   static int[][] map;
+  // dp[i][j] = (i, j)에서 (M - 1, N - 1)까지 가는 경우의 수
   static int[][] dp;
   static int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
@@ -12,21 +13,30 @@ public class Main {
   }
 
   static int dfs(int row, int col) {
+    // 도착 지점이면 1 반환
+    // 도착 지점에서 도착 지점까지 가는 경우는 1가지 밖에 없기 때문
     if (row == M - 1 && col == N - 1) {
       return 1;
     }
+
+    // 이전에 계산된 위치이면 해당 위치의 경우의 수 그대로 반환
     if (dp[row][col] != -1) {
       return dp[row][col];
     }
 
+    // 방문하지 않은 위치면
     dp[row][col] = 0;
     for (int[] d : dir) {
       int nr = row + d[0];
       int nc = col + d[1];
+      // 현재 위치에서 상하좌우 탐색을 했을 때 범위 안이면서 이동할 수 있는 곳이면
       if (isInBound(nr, nc) && map[row][col] > map[nr][nc]) {
+        // 현재 위치에 (이동할 수 있는 위치 -> 도착 지점까지의 경우의 수)를 더함
         dp[row][col] += dfs(nr, nc);
       }
     }
+
+    // 현재 위치로 올 수 있는 모든 경우를 더한 다음 반환하기 때문에 해당 경우의 수는 바뀌지 않음
     return dp[row][col];
   }
 
@@ -44,6 +54,7 @@ public class Main {
       st = new StringTokenizer(br.readLine());
       for (int j = 0; j < N; j++) {
         map[i][j] = Integer.parseInt(st.nextToken());
+        // 방문 여부 초기화
         dp[i][j] = -1;
       }
     }
