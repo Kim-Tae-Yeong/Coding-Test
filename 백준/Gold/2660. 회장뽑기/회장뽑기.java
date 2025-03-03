25936d63b6ac356bf403f70f6f24f782c9bdbe6f
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-03 20:35:59 +0900

    Update 회장뽑기.java

--- 백준/Gold/2660. 회장뽑기/회장뽑기.java
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.java
@@ -1,21 +1,28 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main_2660 {
   static int n, u, v, score, cnt;
+  // distance[i][j] : i번째 회원과 j번째 사람과의 관계(거리)
   static int[][] distance;
   static List<Integer> l = new ArrayList<>();
 
   static void floydWarshall() {
+    // 경유지 선택
     for (int i = 1; i < n + 1; i++) {
+      // 시작 지점
       for (int j = 1; j < n + 1; j++) {
+        // 도착 지점
         for (int k = 1; k < n + 1; k++) {
+          // 시작 지점 or 도착 지점이 경유지면 continue
           if (i == j || i == k) {
             continue;
           }
+          // 시작 지점에서 경유지를 가는 방법이 없으면 or 경유지에서 도착 지점을 가는 방법이 없으면 continue
           if (distance[j][i] == Integer.MAX_VALUE || distance[i][k] == Integer.MAX_VALUE) {
             continue;
           }
+          // 직접 가는 방법과 거쳐서 가는 방법 중 더 작은 방법 선택
           distance[j][k] = Math.min(distance[j][k], distance[j][i] + distance[i][k]);
         }
       }
@@ -30,7 +37,9 @@ public class Main {
     n = Integer.parseInt(st.nextToken());
     distance = new int[n + 1][n + 1];
     for (int i = 0; i < n + 1; i++) {
+      // 초기에는 각 사람들간의 거리를 무한대로 설정
       Arrays.fill(distance[i], Integer.MAX_VALUE);
+      // 자기 자신과의 거리는 0으로 설정
       distance[i][i] = 0;
     }
     while (true) {
@@ -53,6 +62,7 @@ public class Main {
     for (int i = 1; i < n + 1; i++) {
       int tmp = Integer.MIN_VALUE;
       for (int j = 1; j < n + 1; j++) {
+        // i번째 회원 기준으로 각 회원과의 거리 중 가장 먼 거리를 해당 회원의 점수로 설정
         tmp = Math.max(tmp, distance[i][j]);
       }
       if (tmp < score) {
