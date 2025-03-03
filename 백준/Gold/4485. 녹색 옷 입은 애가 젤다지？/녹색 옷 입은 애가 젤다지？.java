56ee5441b45df7e49dd93a7d3185e2fde510fe6c
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-03 14:19:37 +0900

    Update 녹색 옷 입은 애가 젤다지？.java

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,10 +1,11 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main_4485 {
   static int N;
   static int idx = 1;
   static int[][] map;
+  // ans[i][j] : (0, 0)에서 (i, j)로 가는데 잃어버린 최소 금액
   static int[][] ans;
   static PriorityQueue<int[]> pq;
   static int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
@@ -14,6 +15,7 @@ public class Main {
   }
 
   static int dijkstra() {
+    // 잃어버린 금액을 오름차순으로 정렬
     pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
     pq.offer(new int[] { 0, 0, map[0][0] });
     ans[0][0] = map[0][0];
@@ -24,10 +26,12 @@ public class Main {
       int col = info[1];
       int cost = info[2];
 
+      // 도착 지점이면 잃어버린 금액 반환
       if (row == N - 1 && col == N - 1) {
         return cost;
       }
 
+      // 이전에 저장된 값보다 잃어버리는 비용이 크면 continue
       if (ans[row][col] < cost) {
         continue;
       }
@@ -37,8 +41,11 @@ public class Main {
         int nc = col + d[1];
         if (isInBound(nr, nc)) {
           int newCost = cost + map[nr][nc];
+          // (현재까지 잃어버린 비용 + 다음 지점에서 잃어버리는 비용)이 이전에 저장된 값보다 작으면
           if (newCost < ans[nr][nc]) {
+            // 잃어버리는 최소 비용 갱신
             ans[nr][nc] = newCost;
+            // 갱신된 정보를 우선순위 큐에 넣음
             pq.offer(new int[] { nr, nc, newCost });
           }
         }
