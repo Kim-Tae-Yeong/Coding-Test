ee5aed562d1cc6a031d44a1c23d359e20dffb429
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-04 21:22:18 +0900

    Update 최소 스패닝 트리.java

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,9 +1,12 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main_1197 {
+  // cnt : mst의 간선 개수
   static int V, E, cnt, mst;
+  // 간선의 가중치를 오름차순 정렬
   static PriorityQueue<Edge> graph = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
+  // parent[i] : i번째 노드의 부모 노드(i번째 노드와 연결된 노드 중 번호가 가장 작은 노드)
   static int[] parent;
 
   static class Edge {
@@ -18,20 +21,28 @@ public class Main {
 
   static void kruskal() {
     while (true) {
+      // 간선의 개수가 (V - 1)이면 break
       if (cnt == V - 1 || graph.isEmpty()) {
         break;
       }
       Edge e = graph.poll();
+      // 현재 간선의 시작 노드와 도착 노드의 부모가 같을 때 해당 간선을 선택하면 싸이클 발생하기 때문에 continue
       if (parent[e.from] == parent[e.to]) {
         continue;
-      } else if (parent[e.from] < parent[e.to]) {
+      }
+      // 도착 노드의 부모 노드 번호가 더 클 떄
+      else if (parent[e.from] < parent[e.to]) {
+        // 도착 노드와 연결된 노드들(도착 노드와 같은 부모 노드를 갖는 노드들)의 부모 노드를 시작 노드의 부모 노드로 바꿔줌
         int beforeParent = parent[e.to];
         for (int i = 1; i < V + 1; i++) {
           if (parent[i] == beforeParent) {
             parent[i] = parent[e.from];
           }
         }
-      } else {
+      }
+      // 시작 노드의 부모 노드 번호가 더 클 떄
+      else {
+        // 시작 노드와 연결된 노드들(시작 노드와 같은 부모 노드를 갖는 노드들)의 부모 노드를 도착 노드의 부모 노드로 바꿔줌
         int beforeParent = parent[e.from];
         for (int i = 1; i < V + 1; i++) {
           if (parent[i] == beforeParent) {
@@ -63,6 +74,7 @@ public class Main {
     cnt = 0;
     mst = 0;
     parent = new int[V + 1];
+    // 초기 각 노드들의 부모 노드는 자기 자신임
     for (int i = 1; i < V + 1; i++) {
       parent[i] = i;
     }
