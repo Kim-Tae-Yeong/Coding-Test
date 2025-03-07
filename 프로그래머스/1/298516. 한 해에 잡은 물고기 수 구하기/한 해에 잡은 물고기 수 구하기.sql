2ca0a9dddf82ac835aa933898eec2e9819e0a292
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-07 18:03:16 +0900

    [level 1] Title: 한 해에 잡은 물고기 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298516. 한 해에 잡은 물고기 수 구하기/한 해에 잡은 물고기 수 구하기.sql
+++ 프로그래머스/1/298516. 한 해에 잡은 물고기 수 구하기/한 해에 잡은 물고기 수 구하기.sql
@@ -1,6 +1,9 @@
 SELECT
-    COUNT(*) AS FISH_COUNT
+    SUM(
+       CASE
+            WHEN YEAR(TIME) = 2021 THEN 1
+            ELSE 0
+        END
+    ) AS FISH_COUNT
 FROM
     FISH_INFO
-WHERE
-    YEAR(TIME) = 2021
