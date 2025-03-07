46148b739fc274e10011b8f0dcbfa0bb143ced66
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-07 14:39:34 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,16 +1,16 @@
-select
-    i.item_id,
-    i.item_name
-from
-    item_info as i
-    join (
-        select
-            *
-        from
-            item_tree
-        where
-            parent_item_id is null
-    ) as t
-    on i.item_id = t.item_id
-order by
+SELECT
+    T.ITEM_ID,
+    (
+        SELECT
+             I.ITEM_NAME
+        FROM
+            ITEM_INFO AS I
+        WHERE
+            T.ITEM_ID = I.ITEM_ID
+    ) AS ITEM_NAME
+FROM
+    ITEM_TREE AS T
+WHERE
+    T.PARENT_ITEM_ID IS NULL
+ORDER BY
     1
