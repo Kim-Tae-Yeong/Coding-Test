3e470e7d67fc66643826c5bcf75392a3a2f29759
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-07 15:18:49 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,16 +1,16 @@
-select
-    name,
-    datetime
-from
-    animal_ins
-where
-    animal_id not in (
-        select
-            animal_id
-        from
-            animal_outs
+SELECT
+    NAME,
+    DATETIME
+FROM
+    ANIMAL_INS
+WHERE
+    ANIMAL_ID NOT IN (
+        SELECT
+            ANIMAL_ID
+        FROM
+            ANIMAL_OUTS
     )
-order by
-    datetime
-limit
+ORDER BY
+    2
+LIMIT
     3
