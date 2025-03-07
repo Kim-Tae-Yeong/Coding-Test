2f45f90c45ff9aa4db0e5c90d22916f3110b646d
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-07 13:46:53 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,22 +1,26 @@
-select
-    g.score,
-    e.emp_no,
-    e.emp_name,
-    e.position,
-    e.email
-from
-    hr_employees as e
-    join (
-        select
-            emp_no,
-            sum(score) as score
-        from
-            hr_grade
-        group by
-            emp_no
-        order by
-            2 desc
-        limit
-            1
-    ) as g
-    on e.emp_no = g.emp_no
+WITH CTE AS (
+    SELECT
+        EMP_NO,
+        SUM(SCORE) AS SCORE
+    FROM
+        HR_GRADE
+    WHERE
+        YEAR = 2022
+    GROUP BY
+        1
+    ORDER BY
+        2 DESC
+    LIMIT
+        1
+)
+
+SELECT
+    C.SCORE,
+    E.EMP_NO,
+    E.EMP_NAME,
+    E.POSITION,
+    E.EMAIL
+FROM
+    HR_EMPLOYEES AS E
+    JOIN CTE AS C
+    ON E.EMP_NO = C.EMP_NO
