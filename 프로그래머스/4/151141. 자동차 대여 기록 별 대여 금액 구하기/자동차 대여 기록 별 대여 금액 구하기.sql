dfef7b48ba13ad0d8dc1d9a3f763d412ed5950a9
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-07 16:01:02 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,15 +1,43 @@
-SELECT H.HISTORY_ID, 
-       ROUND(C.DAILY_FEE * (DATEDIFF(H.END_DATE, H.START_DATE) + 1) * (1 - IFNULL(D.DISCOUNT_RATE, 0) / 100), 0) AS FEE
-FROM CAR_RENTAL_COMPANY_CAR C
-JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY H
-    ON C.CAR_ID = H.CAR_ID
-LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN D
-    ON C.CAR_TYPE = D.CAR_TYPE
-    AND D.DURATION_TYPE =
-        CASE
-            WHEN DATEDIFF(END_DATE, START_DATE) + 1 >= 90 THEN '90일 이상'
-            WHEN DATEDIFF(END_DATE, START_DATE) + 1 >= 30 THEN '30일 이상'
-            WHEN DATEDIFF(END_DATE, START_DATE) + 1 >= 7 THEN '7일 이상'
-        END 
-WHERE C.CAR_TYPE = '트럭'
-ORDER BY FEE DESC, H.HISTORY_ID DESC;
+SELECT
+    H.HISTORY_ID,
+    CASE
+        WHEN DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 90 THEN FLOOR(C.DAILY_FEE * (100 - (
+            SELECT
+                DISCOUNT_RATE
+            FROM
+                CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+            WHERE
+                CAR_TYPE = '트럭' AND
+                DURATION_TYPE LIKE '90%'
+            )
+        ) / 100 * (DATEDIFF(H.END_DATE, H.START_DATE) + 1))
+        WHEN DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 30 THEN FLOOR(C.DAILY_FEE * (100 - (
+            SELECT
+                DISCOUNT_RATE
+            FROM
+                CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+            WHERE
+                CAR_TYPE = '트럭' AND
+                DURATION_TYPE LIKE '30%'
+            )
+        ) / 100 * (DATEDIFF(H.END_DATE, H.START_DATE) + 1))
+        WHEN DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 7 THEN FLOOR(C.DAILY_FEE * (100 - (
+            SELECT
+                DISCOUNT_RATE
+            FROM
+                CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+            WHERE
+                CAR_TYPE = '트럭' AND
+                DURATION_TYPE LIKE '7%'
+            )
+        ) / 100 * (DATEDIFF(H.END_DATE, H.START_DATE) + 1))
+        ELSE C.DAILY_FEE * (DATEDIFF(H.END_DATE, H.START_DATE) + 1)
+    END AS FEE
+FROM
+    CAR_RENTAL_COMPANY_CAR AS C
+    JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY AS H
+    ON C.CAR_TYPE = '트럭' AND
+    C.CAR_ID = H.CAR_ID
+ORDER BY
+    2 DESC,
+    1 DESC
