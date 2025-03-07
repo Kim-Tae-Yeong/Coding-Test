eaa33f0c0146e638f5a0c833c53f03756f2e253d
Kim-Tae-Yeong <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-03-07 16:20:27 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,25 +1,34 @@
+WITH CTE AS (
+    SELECT
+        WRITER_ID
+    FROM
+        USED_GOODS_BOARD
+    GROUP BY
+        1
+    HAVING
+        COUNT(WRITER_ID) >= 3
+)
+
 SELECT
     U.USER_ID,
     U.NICKNAME,
     CONCAT(
         U.CITY,
-        ' ',
+        " ",
         U.STREET_ADDRESS1,
-        ' ',
+        " ",
         U.STREET_ADDRESS2
     ) AS '전체주소',
     CONCAT(
-        SUBSTRING(U.TLNO, 1, 3), '-',
-        SUBSTRING(U.TLNO, 4, 4), '-',
+        SUBSTRING(U.TLNO, 1, 3),
+        "-",
+        SUBSTRING(U.TLNO, 4, 4),
+        "-",
         SUBSTRING(U.TLNO, 8, 4)
-    ) AS 전화번호
+    ) AS '전화번호'
 FROM
-    USED_GOODS_BOARD AS B
-    JOIN USED_GOODS_USER AS U
-    ON B.WRITER_ID = U.USER_ID
-GROUP BY
-    B.WRITER_ID
-HAVING
-    COUNT(*) >= 3
+    USED_GOODS_USER AS U
+    JOIN CTE AS C
+    ON U.USER_ID = C.WRITER_ID
 ORDER BY
     1 DESC
